ac78d72d3ca8ce275ca8eaa909f6d428bcd7b945
ArielBetti <ariel.betti@gmail.com>
2024-04-22 04:58:01 -0300

    deploy: gh pages config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/dungeon-gamble-devlog",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
