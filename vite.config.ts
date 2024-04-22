a5ef1c49aff7b45cbc9996b154c7f31b89fe4d12
ArielBetti <ariel.betti@gmail.com>
2024-04-22 05:55:13 -0300

    chore: update base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/dungeon-gamble-devlog",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
